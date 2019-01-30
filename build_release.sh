003ce0dd9d917dbcaef2fd3316be3f2286975984
AlexsJones <jonesax@hush.com>
2019-01-30 09:54:38 +0000

    updated build release script

--- build_release.sh
+++ build_release.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -x
 
+git add VERSION
+git commit -m "bumping version to $(cat version)"
+git push origin master
 git tag -a $(cat VERSION) -m "Automated release version: $(cat VERSION)"
 git push origin $(cat VERSION)
-goreleaser
+goreleaser --rm-dist
