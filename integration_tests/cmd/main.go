edceb79cbe56e83a80c3c8e3c0fd4d45bc7f972e
AlexsJones <alexsimonjones@gmail.com>
2019-04-17 14:45:23 +0100

    updatedush

--- integration_tests/cmd/main.go
+++ integration_tests/cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 )
 
+
 func main() {
 
 	awsP := os.Getenv("AWS_DEFAULT_PROFILE")
@@ -36,29 +37,35 @@ func main() {
 	input := &cloudformation.CreateStackInput{TemplateURL:
 		aws.String("https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2019-02-11/amazon-eks-nodegroup.yaml"),
 		StackName: aws.String("eks-deployment-stack")}
-	input.RoleARN = aws.String("arn:aws:iam::976358786557:role/EKSRole")
+
+	delInput := cloudformation.DeleteStackInput{StackName:aws.String("eks-deployment-stack")}
+
+
+
+
+	input.Capabilities = []*string{aws.String("CAPABILITY_IAM")}
 
 	input.Parameters = []*cloudformation.Parameter{{
 		ParameterKey:   aws.String("NodeGroupName"),
-		ParameterValue: aws.String(""),
+		ParameterValue: aws.String("testgroup"),
 	},{
 		ParameterKey:   aws.String("ClusterControlPlaneSecurityGroup"),
 		ParameterValue: aws.String("sg-c76452b6"),
 	},{
 		ParameterKey:   aws.String("KeyName"),
-		ParameterValue: aws.String(""),
+		ParameterValue: aws.String("NodePoolKeyPair"),
 	},{
 		ParameterKey:   aws.String("NodeImageId"),
-		ParameterValue: aws.String(""),
+		ParameterValue: aws.String("ami-0923e4b35a30a5f53"),
 	},{
 		ParameterKey:   aws.String("Subnets"),
 		ParameterValue: aws.String("subnet-7d52a956, subnet-980d99d3, subnet-dec28384, subnet-644e241d"),
 	},{
 		ParameterKey:   aws.String("VpcId"),
-		ParameterValue: aws.String(""),
+		ParameterValue: aws.String("vpc-7789990e"),
 	},{
 		ParameterKey:   aws.String("ClusterName"),
-		ParameterValue: aws.String(""),
+		ParameterValue: aws.String("testcluster"),
 	},
 	}
 
@@ -67,5 +74,6 @@ func main() {
 		fmt.Printf(err.Error())
 		os.Exit(1)
 	}
+
 	fmt.Println(cso.GoString())
 }
